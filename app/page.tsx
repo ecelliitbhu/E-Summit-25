68f3087639e854f39ebbf96d340a2934c897b04b
Vishal K <korada.vishal.phe22@itbhu.ac.in>
2023-12-10 12:09:13 +0530

    page changed to original

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@/components/ui/button";
 import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
 export default function Home() {
   return (
-    <main className="grid items-center pb-8 pt-6 md:py-10 floating-navbar">
-      <Navbar></Navbar>
-      <></>
+    <main className="grid items-center pb-8 pt-6 md:py-10">
+      <Button>Click me</Button>
       <Footer></Footer>
     </main>
   );
