cc38436568aa273287cbb727b9cec2584a6126ed
Ashvita Ramanan <r.ashvita.mec22@itbhu.ac.in>
2023-12-10 20:04:08 +0530

    faq section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client'
 import { Button } from "@/components/ui/button"
+import { useState } from 'react'
+import Faqs from "@/components/faq/Faq"
 
 export default function Home() {
   return (
     <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
-      <Button>Click me</Button>
+        <Button>Click me</Button>
+      <Faqs></Faqs>
     </main> 
   )
 }
