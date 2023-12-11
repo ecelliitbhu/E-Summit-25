ca4dad3d41ca8b788e991440bd4ac634c8cce883
rahulX001 <rahul.kumarsonkar.eee21@itbhu.ac.in>
2023-12-12 02:20:45 +0530

    added faq

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import Faqs from "@/components/faq/Faq"
-import { Button } from "@/components/ui/button";
 import MarqueeComponent from "@/components/sponsorCard/sponsor_card";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <main className="grid items-center  ">
         <Hero/>
-      {/*<Button>Bhai aaj ka kyaa scene hai?</Button>*/}
       <MarqueeComponent></MarqueeComponent>
       <Faqs></Faqs>
       <Footer></Footer>
