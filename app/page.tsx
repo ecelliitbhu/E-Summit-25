d668d5ba49f0a3e989719d7617edd138f5816324
Sanskruti Kolgane <sanskrutikolgane@gmail.com>
2023-12-13 17:49:43 +0530

    Added About Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Faqs from "@/components/faq/Faq"
 import MarqueeComponent from "@/components/sponsorCard/sponsor_card";
+import About from "@/components/about";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
     <main className="grid items-center  ">
         <Hero/>
       <MarqueeComponent></MarqueeComponent>
+      <About></About>
       <Faqs></Faqs>
       <Footer></Footer>
     </main>
