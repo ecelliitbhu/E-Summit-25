97267e60f7efe660209524411f372c79071ba900
rahulX001 <rahul.kumarsonkar.eee21@itbhu.ac.in>
2023-12-15 23:24:54 +0530

    some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,6 @@ export default function RootLayout({
         <div className="flex min-h-screen flex-col">
             <Navbar/>
           <main className="flex-1">
-
               {children}
           </main>
         </div>
