926905d96b6e051a3737df237de444facd60e979
name <you@example.com>
2023-12-16 10:19:15 +0530

    gallery added

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,14 @@ import MarqueeComponent from "@/components/sponsorCard/sponsor_card";
 import About from "@/components/about";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Gallery from "@/components/gallery/gallery"
 export default function Home() {
   return (
     <main className="grid items-center  ">
         <Hero/>
         <MarqueeComponent/>
         <About/>
+        <Gallery />
         <Faqs/>
         <Footer/>
     </main>
