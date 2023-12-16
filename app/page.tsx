3c388d7edb1d041b6c57e1d7984b72648d373921
Eli4479 <patel.aryan.eee21@itbhu.ac.in>
2023-12-16 21:32:47 +0530

    Refactor page.tsx and add The_card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-'use client'
-import Faqs from "@/components/faq/Faq"
+"use client";
+import Faqs from "@/components/faq/Faq";
 import MarqueeComponent from "@/components/sponsorCard/sponsor_card";
 import About from "@/components/about";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
-import Gallery from "@/components/gallery/gallery"
+import Gallery from "@/components/gallery/gallery";
+import The_card from "@/components/The_card";
 export default function Home() {
   return (
     <main className="grid items-center  ">
-        <Hero/>
-        <MarqueeComponent/>
-        <About/>
-        <Gallery />
-        <Faqs/>
-        <Footer/>
+      <Hero />
+      <MarqueeComponent />
+      <About />
+      <Gallery />
+      <Faqs />
+      <The_card />
+      <Footer />
     </main>
   );
 }
-
