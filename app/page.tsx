666db6bbebd5d788647e5ebffd5376ec95fd7297
Arun <arun.kumar.cd.civ21@itbhu.ac.in>
2023-12-17 10:03:14 +0530

    why should you attend-section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 import Footer from "@/components/Footer";
+import WSYA from "@/components/WSYA";
 
 export default function Home() {
   return (
     <main className="grid items-center pb-8 pt-6 md:py-10">
       <Button>Click me</Button>
+      <WSYA></WSYA>
       <Footer></Footer>
     </main>
   );
