fcd84a75ab022d1b8498649834e23ae4137497cb
Veer0x1 <balveer.singhrao.eee21@itbhu.ac.in>
2023-12-27 16:06:52 +0530

    content spacing

--- components/About/about.tsx
+++ components/About/about.tsx
@@ -12,17 +12,18 @@ const About = () => {
           </h3>
           <h1 className={"m-0 text-secondary"}>
             E-Summit is an electrifying celebration of innovation and
-            entrepreneurship, brought to you by the Entrepreneurship
-            Cell(E-Cell) at the Indian Institute of Technology(IIT) BHU.
+            entrepreneurship, brought to you by the Entrepreneurship Cell (
+            E-Cell ) at the Indian Institute of Technology ( IIT ) BHU.
           </h1>
           <p className={"m-0 text-secondary"}>
             This annual festival is a convergence of visionary thinkers, budding
             entrepreneurs,and established industry leaders, all united by a
             common passion. - &quot;The drive to create,innoavte and transform
-            the future.&ldquo; E-Summit is where dreams are nurtured,ideas are
-            cultivated, and future entrepreneurs are born. Join us in this
-            exhilarating journey of entrepreneurship, and together Let&apos;s
-            write the future!
+            the future.&ldquo;
+            <br /> E-Summit is where dreams are nurtured,ideas are cultivated,
+            and future entrepreneurs are born. Join us in this exhilarating
+            journey of entrepreneurship, and together let&apos;s write the
+            future!
           </p>
         </div>
       </div>
