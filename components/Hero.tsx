67c32605234981dbe918cbdd4242c3ab2ac34fca
Shibbu264 <116422972+Shibbu264@users.noreply.github.com>
2025-01-07 15:24:13 +0530

    changed images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { FunctionComponent, useState, useEffect } from "react";
 import Image from "next/image";
-import Logo from "@/public/logos/new-logo.png";
+import Logo from "@/public/logos/esummit25.png";
 import Particle from "@/components/particle";
 import { motion } from "framer-motion";
 import Link from "next/link";
